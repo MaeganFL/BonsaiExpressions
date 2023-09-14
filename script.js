4067bdbb8b9a52f3b8b22bc1470e204e23b6b289
MaeganFL <123469956+MaeganFL@users.noreply.github.com>
2023-09-14 10:09:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+/*  JavaScript 7th Edition
+    Chapter 12
+    Chapter case
+
+    Bonsai Expressions FAQ 
+    Author: Maegan Marlow
+    Date: 09/11/2023  
+
+    Filename: script.js
+*/
+
+"use strict";
+
+// Run once the page is loaded and ready 
+$( () => {
+
+    // Animate the h1 heading 
+    $("section > h1").css({
+        fontSize: 0,  // Changed from semicolon (;) to comma (,)
+        opacity: 0
+    })
+    .animate({
+        fontSize: "2.3em",
+        opacity: 1
+    }, 600);
+
+    // Reveal the questions when th page opens 
+    $("dl#faq")
+    .hide()
+    .effect("clip", {
+        mode: "show",
+        direction: "horizontal"
+    }, 600);
+
+    // Add click events to each question in the FAQ
+    $("dl#faq dt").click( e => {
+
+        // Alternate between hiding and showing the answer
+        let question = $(e.target);
+        let answer = $(question.next());
+
+        $(question).toggleClass("hiddenAnswer");
+
+        if ($(question).hasClass("hiddenAnswer")) {
+            $(answer).slideUp(600);
+        } else {
+            $(answer).slideDown(600);
+        }
+    
+    });
+});
+
